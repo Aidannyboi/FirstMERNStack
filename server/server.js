defd30081c8b023d849cfc427db41d9059256335
Aidan Castellanos <castellanosaidan@gmail.com>
2023-12-06 16:54:27 -0800

    Changed server.js to make app use cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const  express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 require('./routes/person.routes')(app);
 const port = 8000;
 
-app.listen(port, () => console.log('activated on port: ${port}'));
+app.listen(port, () => console.log('activated on port 8000'));
